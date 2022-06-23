723bf377a4851749ff868355575fc7d2f1119538
sam gardner <samgardner20@gmail.com>
2022-06-23 10:25:04 -0400

    got reset and resize to work

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
 const gridContainer = document.querySelector('.grid-container')
 const resizeBtn = document.getElementById('resize')
+const resetBtn = document.getElementById('reset')
+
 let numBoxes = 25
 
-resizeBtn.addEventListener('click', function(){
-
-    let numBoxes = prompt("How big would you like your grid? We can go up to 100X100!. Just type one number")
-    makeGrid(numBoxes)
-
-})
 
 
-// console.log(numBoxes)
 function makeGrid(numBoxes){
+    
     let gridArea = numBoxes *numBoxes
-
-
-   for(let i =0; i < gridArea; i++){
+    
+    
+    for(let i =0; i < gridArea; i++){
         let gridItem = document.createElement('div')
         gridContainer.style.gridTemplateColumns = `repeat(${numBoxes}, 1fr)`
         gridContainer.style.gridTemplateRows = `repeat(${numBoxes}, 1fr)`
-
-        gridContainer.appendChild(gridItem)
-        
-   }
-
-   
-//         let rows = document.createElement('div')
-//         rows.classList.add('row')
-//         for(let j =0; j< numBoxes; j++){
-//             let columns = document.createElement('div')
-//             columns.classList.add('column')
-//             rows.appendChild(columns)
-//         }
-//     gridContainer.appendChild(rows)
-//    }
-
-    const squares = document.querySelectorAll('.column')
-
-
-    squares.forEach((square) => {
-        square.addEventListener('mouseover', function() {
-            square.classList.add('black-bkgd')
+        gridItem.addEventListener('mouseover', function(){
+            gridItem.classList.add('black-bkgd')
         })
-    })
+        gridContainer.appendChild(gridItem)
+    }
 }
 
+resizeBtn.addEventListener('click', function(){
+    let divs = document.querySelectorAll('.grid-container div')
+    reset(divs) 
+
+    let numBoxes = prompt("How big would you like your grid? We can go up to 100X100!. Just type one number")
+    makeGrid(numBoxes)
+    
+})
+resetBtn.addEventListener('click', reset)
+
 makeGrid(numBoxes)
+
+
+function reset(divs){
+    divs = document.querySelectorAll('.grid-container div')
+    for(let i=0; i < divs.length ; i++){
+        divs[i].classList.remove('black-bkgd')
+    }
+}
+
+
